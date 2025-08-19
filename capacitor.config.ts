89165993640ed835642b174adb30d1963c7cd61b
Ranganath P <ranagathgowdap07@gmail.com>
2025-08-19 20:32:00 +0530

    Updated app logo and name

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dignify.deals',
+  appName: 'dignify',
+  webDir: 'build'
+};
+
+export default config;
