6f6fbe8ff4a0d002fb6036c4231ec79e07d962e8
Ranganath P <ranagathgowdap07@gmail.com>
2025-12-23 20:10:38 +0530

    new slight change in checkout page

--- src/pages/CheckoutStep1.js
+++ src/pages/CheckoutStep1.js
@@ -517,7 +517,7 @@ try {
       {/* Details */}
       <div className="payment-details">
         <span className="price">
-          Pay ₹{previewPartialNow} now,   ₹{previewPartialLater} later
+          Pay ₹{previewPartialNow} now, 
         </span>
         <small className="secure-text">Secure payment via Razorpay</small>
       </div>
